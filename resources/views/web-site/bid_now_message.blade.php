1c9ba7d47dfb5c3cab2fd11d2f89a8ce0708fa58
Dawn36 <dawngill08@gmail.com>
2022-11-05 21:47:45 +0500

    Update bid_now_message.blade.php

--- resources/views/web-site/bid_now_message.blade.php
+++ resources/views/web-site/bid_now_message.blade.php
@@ -1,7 +1,14 @@
-<div class="alert alert-danger" role="alert">
+
   @if($orderData[0]->status == 'offer')
-    You have already made an offer on this bid
+  <div class="alert alert-success" role="alert">
+  <p>Thank you for submitting your BID. The Buyer will now ACCEPT OR REJECT your BID. You can use our CHAT feature for further discussion with the buyer</p>
+  </div>
+  
+  <div class="alert alert-danger" role="alert">
+  <p>Kindly Note: Packpal only connect Buyers and Sellers and are not liable legally or non-legally for any mishaps in your business transactions</p>
+  </div>
     @endif
+  <div class="alert alert-danger" role="alert">
     @if($orderData[0]->status == 'inprocess')
     You have already made an offer on this bid and it is in In-process
     @endif
